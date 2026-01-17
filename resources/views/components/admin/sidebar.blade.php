c30721f7dfea4dae736dcd63813a16271b6dbf7f
prakashm-code <prakashm2814@gmail.com>
2026-01-17 14:24:39 +0530

    Update sidebar.blade.php

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -19,7 +19,7 @@
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link {{ in_array(Request::segment(2), ['user', 'add_user', 'edit_user']) ? 'active' : '' }}"
+                    <a class="sidebar-link {{ in_array(Request::segment(2), ['user', 'add_user', 'edit_user','add_user_plans']) ? 'active' : '' }}"
                         href="{{ route('admin.user') }}" aria-expanded="false">
                         <i class="ti ti-user-circle"></i>
                         <span class="hide-menu">Users</span>
